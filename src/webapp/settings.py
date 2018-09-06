d459fb580a56f4b5b0597d7d862e1f9713bef5be
mbwatson <mattwat@gmail.com>
2018-09-06 19:24:18 -0400

    check environment variable for secet key

--- src/webapp/settings.py
+++ src/webapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '--wdnf3ddh1hc)u&m&d)78txleebf)_4r32a50_+0rg(x_eznb'
+SECRET_KEY = os.environ.get('SECRET_KEY', '--wdnf3ddh1hc)u&m&d)78txleebf)_4r32a50_+0rg(x_eznb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
